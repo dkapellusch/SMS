d99f8e01072e004881f2c73b53ec38c3ea1ccb3c
Daniel Kapellusch <dkapellusch@users.noreply.github.com>
2017-10-31 21:59:00 -0700

    blitboat

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls:['./home.component.scss']
 })
 export class HomeComponent {
+    public Hovered :boolean = false;
+    public Hovered2 :boolean = false;
+    public SmsText = "B.L.I.T.B.O.A.T";
+    @ViewChild('sms') SMS :ElementRef;
+
+    public SmsHovered(eventData:Event):void {
+        this.SmsText = "Barnes Lab Is The Best Of All Time!"
+        this.Hovered2 = true;
+    }
+    public SmsUnHovered(eventData:Event):void {
+        this.Hovered2 = false;
+        this.SmsText = "B.L.I.T.B.O.A.T"
+    }
 }
