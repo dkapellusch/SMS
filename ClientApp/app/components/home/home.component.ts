00e60ad0e0a36a7777d99f3c43f9c0bb507967d5
Daniel Kapellusch <dkapellusch@gmail.com>
2017-11-01 09:35:48 -0700

    Updated SMS font and animation

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -1,22 +1,35 @@
-import { Component, ViewChild, HostListener, ElementRef } from '@angular/core';
+import { Component, ViewChild, HostListener, ElementRef,OnInit } from '@angular/core';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html',
     styleUrls:['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
     public Hovered :boolean = false;
     public Hovered2 :boolean = false;
-    public SmsText = "B.L.I.T.B.O.A.T";
+    public SmsText =  "S.M.S.";
+    private _originalText :string = this.SmsText;
+    private _transitionText = "Sample. Management. System."
+
+   
+    ngOnInit(): void {
+        this.SmsText = this._transitionText;
+        this.Hovered2 = true;
+        setTimeout(() => {
+            this.SmsText = this._originalText;
+            this.Hovered2 = false;
+        }, 2500)
+    }
+    
     @ViewChild('sms') SMS :ElementRef;
 
     public SmsHovered(eventData:Event):void {
-        this.SmsText = "Barnes Lab Is The Best Of All Time!"
+        this.SmsText = this._transitionText;
         this.Hovered2 = true;
     }
     public SmsUnHovered(eventData:Event):void {
         this.Hovered2 = false;
-        this.SmsText = "B.L.I.T.B.O.A.T"
+        this.SmsText = this._originalText
     }
 }
